5ddc099a1f88a9e37bfc64479836871afb689dca
Kevin McMinn <kmcminn@bsu.edu>
2018-01-17 20:58:27 -0500

    Copies c# stuff from real project

---
+++ AgorithmTestProject/Course.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgorithmTestProject
+{
+    class Course
+    {
+        public string courseNumber { get; set; }
+
+        public string courseTitle { get; set; }
+
+        public string creditHours { get; set; }
+
+        public bool fall { get; set; }
+        public bool spring { get; set; }
+        public bool summer { get; set; }
+
+        public string[] prerequisites { get; set; }
+
+        public string courseDescription { get; set; }
+    }
+}
